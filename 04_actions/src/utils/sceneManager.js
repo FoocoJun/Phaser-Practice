8e4f2e25749ec5e7069f01757077a6b2890bfc76
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2023-05-07 16:41:19 +0900

    게임 클리어 씬 메니저 생성

--- 04_actions/src/utils/sceneManager.js
+++ 04_actions/src/utils/sceneManager.js
@@ -9,6 +9,17 @@ export function loseGame(playingScene) {
   });
 }
 
+// 게임에서 이겼을 때 이긴 효과음을 재생하고,
+// GameClearScene으로 전환시키는 함수입니다.
+export function winGame(playingScene) {
+  playingScene.m_gameClearSound.play();
+  playingScene.scene.start('gameClearScene', {
+    mobsKilled: playingScene.m_topBar.m_mobsKilled,
+    level: playingScene.m_topBar.m_level,
+    secondElapsed: playingScene.m_secondElapsed,
+  });
+}
+
 export function restartGame(GameOverScene) {
   GameOverScene.scene.start('playGame');
 }
@@ -16,4 +27,5 @@ export function restartGame(GameOverScene) {
 export default {
   loseGame,
   restartGame,
+  winGame,
 };
