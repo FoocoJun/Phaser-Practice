7d19517d865046bc15eb37c85547725cc62cf29e
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2023-05-07 16:24:48 +0900

    게임오버 씬 고도화 (각 변수 전달 by init)

--- 04_actions/src/scenes/GameOverScene.js
+++ 04_actions/src/scenes/GameOverScene.js
@@ -1,10 +1,20 @@
 import Phaser from 'phaser';
 import Config from '../Config';
 import { gameOver } from '../utils/restartManager';
+import Button from '../ui/Button';
+import { getTimeString } from '../utils/time';
 
 export default class GameOverScene extends Phaser.Scene {
   constructor() {
     super('gameOverScene');
+  }
+
+  // 데이터를 전달받기 위해 init 메서드를 사용해줍니다.
+  init(data) {
+    // 받아온 데이터를 멤버 변수로 저장해줍니다.
+    this.m_mobsKilled = data.mobsKilled;
+    this.m_level = data.level;
+    this.m_secondElapsed = data.secondElapsed;
   }
 
   create() {
@@ -21,21 +31,52 @@ export default class GameOverScene extends Phaser.Scene {
     this.add
       .bitmapText(
         Config.width / 2,
-        Config.height / 2,
+        Config.height / 2 - 180,
         'pixelFont',
         'Game Over',
         80
       )
       .setOrigin(0.5);
 
+    // 플레이 시간을 표시해줍니다.
     this.add
       .bitmapText(
         Config.width / 2,
-        Config.height / 1.7,
+        Config.height / 2 - 30,
         'pixelFont',
-        'Press "Enter" to restart',
+        `You survived for ${getTimeString(this.m_secondElapsed)} !`,
         40
       )
       .setOrigin(0.5);
+
+    // 잡은 몹 수와 레벨을 표시해줍니다.
+    this.add
+      .bitmapText(
+        Config.width / 2,
+        Config.height / 2 + 30,
+        'pixelFont',
+        `Mobs Killed : ${this.m_mobsKilled}, Level : ${this.m_level}`,
+        40
+      )
+      .setOrigin(0.5);
+
+    // 메인 화면으로 이동하는 버튼을 추가해줍니다.
+    new Button(
+      Config.width / 2,
+      Config.height / 2 + 180,
+      'Go to Main',
+      this,
+      () => this.scene.start('mainScene')
+    );
+
+    this.add
+      .bitmapText(
+        Config.width / 2,
+        Config.height / 2 + 250,
+        'pixelFont',
+        'or Press "Enter" to restart',
+        40
+      )
+      .setOrigin();
   }
 }
--- 04_actions/src/utils/sceneManager.js
+++ 04_actions/src/utils/sceneManager.js
@@ -2,7 +2,11 @@
 // GameOverScene으로 전환시키는 함수입니다.
 export function loseGame(playingScene) {
   playingScene.m_gameOverSound.play();
-  playingScene.scene.start('gameOverScene');
+  playingScene.scene.start('gameOverScene', {
+    mobsKilled: playingScene.m_topBar.m_mobsKilled,
+    level: playingScene.m_topBar.m_level,
+    secondElapsed: playingScene.m_secondElapsed,
+  });
 }
 
 export function restartGame(GameOverScene) {
